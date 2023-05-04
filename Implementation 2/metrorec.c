d4bf197aaa3bc56a23c986bf89d6a762504626d7
C4BrrT <ccbc@cesar.school>
2023-05-04 19:00:03 -0300

    added gitignore, correcting cond call

--- Implementation 2/metrorec.c
+++ Implementation 2/metrorec.c
@@ -22,18 +22,18 @@ void estacao_init(struct estacao *estacao) {
 }
 
 void estacao_preecher_vagao(struct estacao * estacao, int assentos) {
-    pthread_cond_wait
-    pthread_cond_broadcast
+    //pthread_cond_wait
+    //pthread_cond_broadcast
 }
 
 void estacao_espera_pelo_vagao(struct estacao * estacao) {
     pthread_mutex_lock(&estacao->mutex);
     while(estacao->passageiros > 0){
-        estacao->passageiros = pthread_cond_wait(&cond, &mutex);
+        estacao->passageiros = pthread_cond_wait(&estacao->cond, &estacao->mutex);
     }
 }
 
 void estacao_embarque(struct estacao * estacao) {
-    estacao->passageiros = pthread_cond_signal(&cond);
+    estacao->passageiros = pthread_cond_signal(&estacao->cond);
     pthread_mutex_unlock(&estacao->mutex);
 }
