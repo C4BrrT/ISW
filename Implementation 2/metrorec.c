7cdd154878e8bcaae1f0350a1b81ee0693f1e118
C4BrrT <ccbc@cesar.school>
2023-05-15 20:07:16 -0300

    deleted printf

--- Implementation 2/metrorec.c
+++ Implementation 2/metrorec.c
@@ -30,9 +30,7 @@ void estacao_init(struct estacao *estacao) {
 
 void estacao_preencher_vagao(struct estacao * estacao, int assentos) {
     pthread_mutex_lock(&estacao->mutex);
-    //printf("Vagão chegando\n");
     estacao->livres = assentos;
-    //printf("Faltam %d passageiros embarcarem\n", estacao->passageiros);
     pthread_cond_broadcast(&estacao->cond_p);
     while(estacao->livres != estacao->embarcados_emb && estacao->passageiros_pas != 0){
         pthread_cond_wait(&estacao->cond_v, &estacao->mutex);
@@ -41,7 +39,6 @@ void estacao_preencher_vagao(struct estacao * estacao, int assentos) {
     estacao->embarcados=0;
     estacao->embarcados_emb=0;
     pthread_cond_signal(&estacao->cond_e_v);
-    //printf("Vagao preenchido\n");
     pthread_mutex_unlock(&estacao->mutex);
 }
 
@@ -49,11 +46,9 @@ void estacao_espera_pelo_vagao(struct estacao * estacao) {
     pthread_mutex_lock(&estacao->mutex);
     estacao->passageiros++;
     estacao->passageiros_pas++;
-    //printf("Passageiro %d começou a esperar\n", estacao->passageiros);
     while(estacao->livres == estacao->embarcados || (estacao->embarcados == 0 && estacao->livres == 0)){
         pthread_cond_wait(&estacao->cond_p, &estacao->mutex);
     }
-    //printf("Passageiro %d saiu da espera\n", estacao->passageiros);
     estacao->embarcados++;
     estacao->passageiros--;
     pthread_mutex_unlock(&estacao->mutex);
@@ -62,10 +57,8 @@ void estacao_espera_pelo_vagao(struct estacao * estacao) {
 void estacao_embarque(struct estacao * estacao) {
     pthread_mutex_lock(&estacao->mutex);
     estacao->embarcados_emb++;
-    //printf("Agora tem %d assentos livres no vagão, e %d passageiros embarcaram\n", estacao->livres, estacao->embarcados);
     estacao->passageiros_pas--;
     if(estacao->embarcados_emb == estacao->livres || estacao->passageiros_pas == 0){
-        //printf("signal vagao");
         pthread_cond_signal(&estacao->cond_v);
         pthread_cond_wait(&estacao->cond_e_v, &estacao->mutex);
     }
